009932c0e7cce0d8b2ba031d1d860114c41f6bf4
Yanis Kolli <user@debian.arcahe.ovh>
2020-10-02 23:56:05 +0200

    Ajout de constructeurs et méthodes.

--- Fraction.java
+++ Fraction.java
@@ -1,2 +1,21 @@
-public class fraction {
+public class Fraction {
+    private int numerateur;
+    private int denominateur;
+    
+    public int getNum(){return numerateur;}
+    
+    public int getDenom(){return denominateur;}
+
+    public void setNum(int num){this.numerateur=num;}
+
+    public void setDenom(int denom){this.denominateur=denom;}
+
+    public Fraction(int num , int denom){
+        this.numerateur=num;
+        this.denominateur=denom;
+    }
+    public String toString(){
+        return "Numerateur = " + numerateur + " || Denominateur = "+ denominateur ;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
-public class Main
-{
-    public static void main(String[] args)
-     {System.out.println("Premier affichage simple!");
-     }
+public class Main {
+    public static void main(String[] args)throws Exception{
+        System.out.println("Premier affichage simple!");
+        Fraction f =  new Fraction(0,0);
+        f.setNum(4);
+        f.setDenom(5);
+        String var = f.toString();
+        System.out.println(var);
+    }
 }
